47d44eecf28ed80c988d5975b4b2881517ec9925
曹文龙 <1225448773@qq.com>
2020-03-31 15:47:33 +0800

    complete

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
-# unit
-phpunit for testing 
-=======
-# 1225448773-qq.com
-unit
->>>>>>> 534f5a7899724374a6e8270189be8d3bc259b603
+ipcs phpunit
--- tests/test.php
+++ tests/test.php
@@ -443,16 +443,315 @@ class test extends TestCase
                 'cookie: IPCS-SESSIONID='.$code[2]
             ]
         );
-        $subset =
+        $subset = array('depId','depName','counts');
         $this->assertEquals("200", $res['status'], $res['msg']);
     }
 
     /**
-     * 区域查车  根据区域和事件查询车辆
+     * 区域查车  获取车辆指定时间内的轨迹
      * @depends testVerifyCode
      */
-    public function test
+    public function testVehiclesTrips($code){
+        $start_time = '';
+        $end_time = '';
+        $helper = new Helper();
+        $res = $helper->getUrl(
+            $url = self::Ip . 'zeus/api/v1/vehicles/:id/trips?&startTime='.$start_time.'&endTime='.$end_time,
+            [
+                'cookie: IPCS-SESSIONID='.$code[2]
+            ]
+        );
+        $subset = array('vId','plateNo','depId','depName','trips');
+        $this->assertEquals("200", $res['status'], $res['msg']);
+    }
 
+    /**
+     * 音视频  实时播放摄像头信息
+     * @depends testVerifyCode
+     */
+    public function testLive($code){
+        $index = '';
+        $type = '';
+        $helper = new Helper();
+        $res = $helper->postUrl(
+            $url = self::Ip . 'zeus/api/v1/vehicles/:id/live',
+            ['index'=>$index,'type'=>$type],
+            [
+                'cookie: IPCS-SESSIONID='.$code[2]
+            ]
+        );
+        $subset = array('source','name','index');
+        $this->assertEquals("200", $res['status'], $res['msg']);
+    }
+
+    /**
+     * 音视频  停止实时播放
+     * @depends testVerifyCode
+     */
+    public function testStopLive($code){
+        $channel = '';
+        $helper = new Helper();
+        $res = $helper->postUrl(
+            $url = self::Ip . 'zeus/api/v1/vehicles/:id/stop-live',
+            ['channel'=>$channel],
+            [
+                'cookie: IPCS-SESSIONID='.$code[2]
+            ]
+        );
+        $this->assertEquals("200", $res['status'], $res['msg']);
+    }
+
+    /**
+     * 视频回放  获取服务器上存储的视频文件
+     * @depends testVerifyCode
+     */
+    public function testVideos($code){
+        $type = '';
+        $start_time = '';
+        $end_time = '';
+        $channel = '';
+        $storage_type = '';
+        $url = self::Ip . 'zeus/api/v1/vehicles/:id/videos?&type='.$type.'&startTime='.$start_time.'&endTime='.$end_time;
+        if (!empty($channel)){
+            $url .= '&channel='.$channel;
+        }
+        if (!empty($storage_type)){
+            $url .= '&storageType='.$storage_type;
+        }
+        $helper = new Helper();
+        $res = $helper->getUrl(
+            $url,
+            [
+                'cookie: IPCS-SESSIONID='.$code[2]
+            ]
+        );
+        $subset = array('id','startTime','endTime','uploadTime','fileType','channel','size','storageType','path','downloadUrl','uploadType');
+        $this->assertEquals("200", $res['status'], $res['msg']);
+    }
+
+    /**
+     * 视频回放  获取文件上传状态
+     * @depends testVerifyCode
+     */
+    public function testFileTaskId($code){
+        $helper = new Helper();
+        $res = $helper->getUrl(
+            self::Ip . 'zeus/api/v1/file-tasks/:id',
+            [
+                'cookie: IPCS-SESSIONID='.$code[2]
+            ]
+        );
+        $subset = array('status','errorMessage');   // errorMessage 可选参数
+        $this->assertEquals("200", $res['status'], $res['msg']);
+    }
+
+    /**
+     * 视频回放  （暂停/继续/取消）文件上传
+     * @depends testVerifyCode
+     */
+    public function testFileTasksStatus($code){
+        $action = '';
+        $helper = new Helper();
+        $res = $helper->putUrl(
+            self::Ip . 'zeus/api/v1/file-tasks/:id/status',
+            ['action'=>$action],
+            [
+                'cookie: IPCS-SESSIONID='.$code[2]
+            ]
+        );
+        $subset = array('status','errorMessage','path','downloadUrl');   // status 可选参数
+        $this->assertEquals("200", $res['status'], $res['msg']);
+    }
+
+    /**
+     * 视频回放  通知终端回放视频
+     * @depends testVerifyCode
+     */
+    public function testPlayVideo($code){
+        $file_id = ''; //id
+        $start_time = '';
+        $type = '';
+        $helper = new Helper();
+        $res = $helper->getUrl(
+            self::Ip . 'zeus/api/v1/vehicles/:id/play-video?&fileId='.$file_id.'&startTime='.$start_time.'&type='.$type,
+            [
+                'cookie: IPCS-SESSIONID='.$code[2]
+            ]
+        );
+        $subset = array('source','name','index');
+        $this->assertEquals("200", $res['status'], $res['msg']);
+    }
+
+    /**
+     * 视频回放  终端视频回放控制
+     * @depends testVerifyCode
+     */
+    public function testPlayStatus($code){
+        $file_id = '';
+        $action = '';
+        $helper = new Helper();
+        $res = $helper->putUrl(
+            self::Ip . 'zeus/api/v1/vehicles/:id/play-status`',
+            ['fileId'=>$file_id,'action'=>$action],
+            [
+                'cookie: IPCS-SESSIONID='.$code[2]
+            ]
+        );
+        // {
+        //     message: "视频暂停成功"
+        //  }
+        $this->assertEquals("200", "200", $res['msg']);
+    }
+
+    /**
+     * 视频回放  获取全部车辆位置信息
+     * @depends testVerifyCode
+     */
+    public function testAllVehicle($code){
+        $helper = new Helper();
+        $res = $helper->getUrl(
+            self::Ip . 'zeus/api/v1/locations/all-vehicle',
+            [
+                'cookie: IPCS-SESSIONID='.$code[2]
+            ]
+        );
+        $subset = array('vId','plateNo','depId','lat','lng','isMove','isAlert','areaCode');
+        $this->assertEquals("200", $res['status'], $res['msg']);
+    }
+
+    /**
+     * 视频回放  获取车辆位置信息缓存文件
+     * @depends testVerifyCode
+     */
+    public function testCacheVehicles($code){
+        $helper = new Helper();
+        $res = $helper->getUrl(
+            self::Ip . 'zeus/api/v1/caches/vehicles',
+            [
+                'cookie: IPCS-SESSIONID='.$code[2]
+            ]
+        );
+        $subset = array('filePath');
+        $this->assertEquals("200", $res['status'], $res['msg']);
+    }
+
+    /**
+     * 视频回放  获取行政区域
+     * @depends testVerifyCode
+     */
+    public function testAreaCodes($code){
+        $helper = new Helper();
+        $res = $helper->getUrl(
+            self::Ip . 'zeus/api/v1/area-codes',
+            [
+                'cookie: IPCS-SESSIONID='.$code[2]
+            ]
+        );
+        $subset = array('areaCode','name');
+        $this->assertEquals("200", $res['status'], $res['msg']);
+    }
+
+    /**
+     * 视频回放  获取行政区域
+     * @depends testVerifyCode
+     */
+    public function testStatistics($code){
+        $helper = new Helper();
+        $res = $helper->getUrl(
+            self::Ip . 'zeus/api/v1/statistics/dep-vehicles',
+            [
+                'cookie: IPCS-SESSIONID='.$code[2]
+            ]
+        );
+        $subset = array('depId','depName','count','ratio');
+        $this->assertEquals("200", $res['status'], $res['msg']);
+    }
+
+    /**
+     * 入网车辆排行——按区域统计
+     * @depends testVerifyCode
+     */
+    public function testAreaRank($code){
+        $depid = '';
+        $area_code = '';
+        $helper = new Helper();
+        $res = $helper->getUrl(
+            self::Ip . 'zeus/api/v1/statistics/dep-vehicles/area-rank?&depId='.$depid.'&areaCode='.$area_code,
+            [
+                'cookie: IPCS-SESSIONID='.$code[2]
+            ]
+        );
+        $subset = array('areaCode','areaName','count','ratio');
+        $this->assertEquals("200", $res['status'], $res['msg']);
+    }
+
+    /**
+     * 入网车辆排行——当日报警排行榜
+     * @depends testVerifyCode
+     */
+    public function testStatisticsCurrentAlerts($code){
+        $helper = new Helper();
+        $res = $helper->getUrl(
+            self::Ip . 'zeus/api/v1/statistics/current-alerts',
+            [
+                'cookie: IPCS-SESSIONID='.$code[2]
+            ]
+        );
+        $subset = array('areaCode','areaName','count','ratio');
+        $this->assertEquals("200", $res['status'], $res['msg']);
+    }
+
+    /**
+     * 单个组织当日报警排行——按区域
+     * @depends testVerifyCode
+     */
+    public function testStatisticsAreaRank($code){
+        $depid = '';
+        $area_code = '';
+        $helper = new Helper();
+        $res = $helper->getUrl(
+            self::Ip . 'zeus/api/v1/statistics/current-alerts?&depId='.$depid.'&areaCode='.$area_code,
+            [
+                'cookie: IPCS-SESSIONID='.$code[2]
+            ]
+        );
+        $subset = array('areaCode','areaName','vehicleCount','ratio');
+        $this->assertEquals("200", $res['status'], $res['msg']);
+    }
+
+    /**
+     * 单个组织当日报警排行——获取报警详情
+     * @depends testVerifyCode
+     */
+    public function testCurrentAlertsDetail($code){
+        $depid = '';
+        $alert_type_id = '';
+        $area_code = '';
+        $helper = new Helper();
+        $res = $helper->getUrl(
+            self::Ip . 'zeus/api/v1/current-alerts/detail&depId='.$depid.'&alertTypeId='.$alert_type_id.'&areaCode='.$area_code,
+            [
+                'cookie: IPCS-SESSIONID='.$code[2]
+            ]
+        );
+        $subset = array('areaCode','areaName','counts');
+        $this->assertEquals("200", $res['status'], $res['msg']);
+    }
+
+    /**
+     * 单个组织当日报警排行——获取单个报警详情
+     * @depends testVerifyCode
+     */
+    public function testAlertId($code){
+        $helper = new Helper();
+        $res = $helper->getUrl(
+            self::Ip . 'zeus/api/v1/alerts/:id',
+            [
+                'cookie: IPCS-SESSIONID='.$code[2]
+            ]
+        );
+        $this->assertEquals("200", $res['status'], $res['msg']);
+    }
 
 }
 
